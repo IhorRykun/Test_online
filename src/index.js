ef285ee3b4b47e7a15bc33063dab7c4d0fc751ff
IhorRykun <IhorRykun@gmail.com>
2024-02-13 11:33:50 +0200

    update

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import './js/button_wave';
-import './js/button__more';
-import './js/popap';
-import './js/modal';
-import './js/slick';
-import './js/burger-menu';
--- src/js/burger-menu.js
+++
@@ -1,7 +0,0 @@
-const btnBurger = document.querySelector('[data-menu-button]');
-const modal = document.querySelector('[data-menu]');
-
-btnBurger.addEventListener('click', () => {
-  modal.classList.toggle('is-open');
-
-});
